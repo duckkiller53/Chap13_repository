8131a05f1f44f81992138f3b54f557689c168b1b
duckkiller53 <duckkiller53@gmail.com>
2016-01-05 14:23:49 -0600

    Fixing Initial Upload

---
+++ DaveOauth2/grokSwiftREST/DetailViewController.swift
@@ -0,0 +1,91 @@
+//
+//  DetailViewController.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2015-10-20.
+//  Copyright © 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var gist: Gist? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+    
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detailsView = self.tableView {
+            detailsView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source and delegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        } else {
+            return gist?.files?.count ?? 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "About"
+        } else {
+            return "Files"
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        if indexPath.section == 0 {
+            if indexPath.row == 0 {
+                cell.textLabel?.text = gist?.description
+            } else if indexPath.row == 1 {
+                cell.textLabel?.text = gist?.ownerLogin
+            }
+        } else {
+            if let file = gist?.files?[indexPath.row] {
+                cell.textLabel?.text = file.filename
+            }
+        }
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 1 {
+            if let file = gist?.files?[indexPath.row],
+                urlString = file.raw_url,
+                url = NSURL(string: urlString) {
+                    let safariViewController = SFSafariViewController(URL: url)
+                    safariViewController.title = file.filename
+                    self.navigationController?.pushViewController(safariViewController, animated: true)
+            }
+        }
+    }
+}
+
